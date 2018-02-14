9856c7fb511bc7e7eccfd1dd2aa072af21c40af3
Cameron Anderson and Stephanie Faber <cameron@>
2018-02-14 08:55:00 -0800

    add and test set and get input for player2

--- RockPaper.Tests/ModelsTest/GameTest.cs
+++ RockPaper.Tests/ModelsTest/GameTest.cs
@@ -19,5 +19,19 @@ namespace RockPaper.Models.Tests
         //Assert
         Assert.AreEqual(player1Input, newGame.GetPlayer1());
     }
+
+    [TestMethod]
+    public void Player2Input_ReturnInput()
+    {
+        //Arrange
+        int player1Input = 1;
+        int player2Input = 2;
+
+        //Act
+        Game newGame = new Game(1, 2);
+        
+        //Assert
+        Assert.AreEqual(player2Input, newGame.GetPlayer2());
+    }
   }
 }
